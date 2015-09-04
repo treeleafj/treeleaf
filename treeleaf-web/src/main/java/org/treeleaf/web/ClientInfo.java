0ab6fc9832caef4aeec189333b3d5d6a53c64b86
leaf <jlh20l7@163.com>
2015-09-04 19:20:58 +0800

    添加客户端信息自动注入

---
+++ treeleaf-web/src/main/java/org/treeleaf/web/ClientInfo.java
@@ -0,0 +1,19 @@
+package org.treeleaf.web;
+
+/**
+ * @Author leaf
+ * 2015/9/4 0004 18:58.
+ */
+public class ClientInfo {
+
+
+    private String ip;
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
---
+++ treeleaf-web/src/main/java/org/treeleaf/web/spring/handler/ClientInfoHandlerMethodArgumentResolver.java
@@ -0,0 +1,48 @@
+package org.treeleaf.web.spring.handler;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.treeleaf.web.ClientInfo;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author leaf
+ * 2015/9/4 0004 18:52.
+ */
+public class ClientInfoHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static Logger log = LoggerFactory.getLogger(ClientInfoHandlerMethodArgumentResolver.class);
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return ClientInfo.class.equals(methodParameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        String ip = nativeWebRequest.getHeader("X-Real-IP");
+        if (StringUtils.isBlank(ip)) {
+            HttpServletRequest request = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
+            log.info("request:{},{}", request, nativeWebRequest.getNativeRequest());
+            if (request != null) {
+                ip = request.getRemoteAddr();
+            }
+        }
+
+        if ("0:0:0:0:0:0:0:1".equals(ip)) {
+            ip = "127.0.0.1";
+        }
+
+        ClientInfo clientInfo = new ClientInfo();
+        clientInfo.setIp(ip);
+
+        return clientInfo;
+    }
+}
