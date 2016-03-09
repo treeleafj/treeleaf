e4a1526dbbb369ef36785c8868b39587103ae569
姚树洪 <yaoshuhong@iboxpay.com>
2016-03-09 09:58:40 +0800

    添加支持多个spring mvc拦截器的适配类

--- treeleaf-web/src/main/java/org/treeleaf/web/spring/interceptor/MultipleHandlerInerceptor.java
+++ treeleaf-web/src/main/java/org/treeleaf/web/spring/interceptor/MultipleHandlerInerceptor.java
@@ -1,5 +1,7 @@
 package org.treeleaf.web.spring.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,13 +11,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 提供聚合读个HandlerInterceptor的方法
+ * 提供聚合读个HandlerInterceptor的适配方法
  * <p>
  * Created by yaoshuhong on 2015/5/5.
  */
 public class MultipleHandlerInerceptor implements HandlerInterceptor {
 
     private List<HandlerInterceptor> handlers = new ArrayList<>();
+
+    private static Logger log = LoggerFactory.getLogger(MultipleHandlerInerceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -31,15 +35,25 @@ public class MultipleHandlerInerceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        for (HandlerInterceptor item : handlers) {
-            item.postHandle(request, response, handler, modelAndView);
+        for (int i = handlers.size() - 1; i >= 0; i--) {
+            HandlerInterceptor item = handlers.get(i);
+            try {
+                item.postHandle(request, response, handler, modelAndView);
+            } catch (Exception e) {
+                log.error("", e);
+            }
         }
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        for (HandlerInterceptor item : handlers) {
-            item.afterCompletion(request, response, handler, ex);
+        for (int i = handlers.size() - 1; i >= 0; i--) {
+            HandlerInterceptor item = handlers.get(i);
+            try {
+                item.afterCompletion(request, response, handler, ex);
+            } catch (Exception e) {
+                log.error("", e);
+            }
         }
     }
 
