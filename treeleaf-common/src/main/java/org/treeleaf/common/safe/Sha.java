8c6623c18ed14940662976117934be96f75a6668
leaf <jlh20l7@163.com>
2016-05-18 00:23:32 +0800

    修改sha算法

--- treeleaf-common/src/main/java/org/treeleaf/common/safe/Sha.java
+++ treeleaf-common/src/main/java/org/treeleaf/common/safe/Sha.java
@@ -83,4 +83,22 @@ public class Sha {
         }
     }
 
+    /**
+     * 将byte数据转为16进制
+     *
+     * @param bts
+     * @return
+     */
+    public static String bytes2Hex(byte[] bts) {
+        String des = "";
+        String tmp = null;
+        for (int i = 0; i < bts.length; i++) {
+            tmp = (Integer.toHexString(bts[i] & 0xFF));
+            if (tmp.length() == 1) {
+                des += "0";
+            }
+            des += tmp;
+        }
+        return des;
+    }
 }
