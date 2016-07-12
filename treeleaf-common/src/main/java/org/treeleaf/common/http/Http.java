ddd18d665fcec132fae047ee5b0651bb0c352f6b
yaoshuhong <yaoshuhong@shengzhidai.com>
2016-07-12 11:35:33 +0800

    修改common-lang为3

--- treeleaf-common/src/main/java/org/treeleaf/common/http/Http.java
+++ treeleaf-common/src/main/java/org/treeleaf/common/http/Http.java
@@ -1,6 +1,6 @@
 package org.treeleaf.common.http;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.net.ssl.*;
 import java.io.OutputStream;
--- treeleaf-common/src/main/java/org/treeleaf/common/http/Post.java
+++ treeleaf-common/src/main/java/org/treeleaf/common/http/Post.java
@@ -1,7 +1,7 @@
 package org.treeleaf.common.http;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
