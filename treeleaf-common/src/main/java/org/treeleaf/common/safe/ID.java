2a553e5aef8989e6859100e2a486c1863cb26554
yaoshuhong <yaoshuhong@shengzhidai.com>
2016-11-10 14:38:12 +0800

    删除ehcache部分的代码,然后ID增加生成24位的方法

--- treeleaf-common/src/main/java/org/treeleaf/common/safe/ID.java
+++ treeleaf-common/src/main/java/org/treeleaf/common/safe/ID.java
@@ -6,12 +6,20 @@ package org.treeleaf.common.safe;
 public abstract class ID {
 
     /**
-     * 生成一个有序,分布式唯一,且16位长度的id
+     * 生成一个有序,分布式唯一,且24位长度的id(只包含数字和字母,字母都为小写字母)
      *
      * @return
      */
     public static String get() {
-        return Base64.encodeURLSafe(ObjectId.get().toByteArray());
+        return org.apache.commons.codec.binary.Hex.encodeHexString(ObjectId.get().toByteArray());
     }
 
+    /**
+     * 生成一个有序,分布式唯一,且16位长度的短id(url安全的,区分大小写,包含"-")
+     *
+     * @return
+     */
+    public static String getShort() {
+        return Base64.encodeURLSafe(ObjectId.get().toByteArray());
+    }
 }
